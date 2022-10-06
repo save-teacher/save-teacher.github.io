efaa22ed11833b5dbe16a056b714aa11d3acf927
save-teacher <66149312+save-teacher@users.noreply.github.com>
2022-10-06 21:45:21 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,10 +1,10 @@
 var addata = [];
-addata[1]={'title': '广告位招租啦', 'excerpt': '详情请看...', 'href': '404.html'}
+addata[1]={'title': '广告位招租啦', 'excerpt': '详情请看...', 'address': '404.html'}
 function ad(){
   var choose = addata[Math.floor(Math.random()*addata.length)];
   return '\n\
   <li class="post">\n\
-      <h4><a href="'+choose['href']+'">'+choose['title']+'</a></h4>\n\
+      <h4><a href="'+choose['address']+'">'+choose['title']+'</a></h4>\n\
       <p></p><p>'+choose['excerpt']+'</p>\n\
 \n\
 <p></p><p style="font-size: 0.01px;">广告</p>\n\
