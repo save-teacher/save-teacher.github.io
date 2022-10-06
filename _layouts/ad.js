13c908b61651ff1130e59d52cfcf5408db21ff49
save-teacher <66149312+save-teacher@users.noreply.github.com>
2022-10-06 21:19:06 +0800

    Update and rename old.html to ad.js

---
+++ _layouts/ad.js
@@ -0,0 +1,8 @@
+function ad(){
+  return '\n\
+  <li class="post">\n\
+      <h4><a href="/2022/10/06/version1.2.1.html">Save Teacher v1.2.1 正式版 发布</a></h4>\n\
+      <p></p><p>支持如极域等多种电子教室，防止脱控。防止学生不认真上课。禁用注册表、注销、运行等功能。</p>\n\
+\n\
+<p></p>\n\
+    </li>';
